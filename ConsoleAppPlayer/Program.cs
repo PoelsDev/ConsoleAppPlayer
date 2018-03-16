4f27eae5768c55ad53731895b63c2c8d4b020285
Poels Thibault <thibault.poels@immalle.eu>
2018-03-16 11:16:41 +0100

    Add project files.

---
+++ ConsoleAppPlayer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
